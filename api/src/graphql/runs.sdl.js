36fbedfe9150a1d67d120586ae38a27268185906
YokoTori123987 <nongtaop1@gmail.com>
2022-12-19 09:36:34 +0700

    edit return sql run to done

--- api/src/graphql/runs.sdl.js
+++ api/src/graphql/runs.sdl.js
@@ -15,7 +15,7 @@ export const schema = gql`
     runs: [Run!]! @requireAuth
     run(id: String!): Run @requireAuth
     findFirstRun(userId: String!): [Run!]! @requireAuth
-    findTotalOfSumRun(userId: String!): totalDistance! @skipAuth
+    findTotalOfSumRun(userId: String!): String! @skipAuth
   }
 
   input CreateRunInput {
